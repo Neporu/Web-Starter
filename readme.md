a593ea3b0f62ca89ef4123f0f9ee60938f4be0ef
Neporu <incingfux@gmail.com>
2024-10-26 13:36:10 +0700

    Fix the Nav flicker when scroll

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Project Starter
+
+You can use this for your starter Web Project.
+
+this starter project has parcer and MVC Architecture
+most of it the unused JS is just an empy JS file & the rest of it is for you to working on your own project.
+
+Dont forgot to "npm i" to install the JS Library
+
+here's my recommended VSCode extention :
+
+1. Template LIteral Editor
+2. Prettier
+3. html tag wrapper (find the best one)
+4. font awesome auto complete & Preview
+5. ESLint (idk about this one but good to go)
+6. Color Highlight (good for color preview right from the code)
+
+also dont forgot to delete the section tag in HTML cuz they're just a filler
+
+The rest is up to you so good luck with your project...
+
+Happy Coding :D
+
+NOTE: Please put your own favicon in "./src/img/favicon.png" if there's isn't any img folder then create it yourself
--- src/JS/controller.js
+++ src/JS/controller.js
@@ -1,30 +1,36 @@
-import "bootstrap";
-import nav from "./Views/nav.js";
-import footer from "./Views/footer.js";
-import hero from "./Views/hero.js";
+import 'bootstrap';
+import nav from './Views/nav.js';
+import footer from './Views/footer.js';
+import hero from './Views/hero.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime.js';
 
 const controlObserverNav = function () {
-  const header = document.querySelector("header");
+  const header = document.querySelector('header');
+  const hero = document.querySelector('#hero');
   const option = {
     root: null,
     threshold: 0,
+    rootMargin: '-90px',
   };
-  const callback = (entries) => {
+  const callback = entries => {
     const [entry] = entries;
 
     !entry.isIntersecting
-      ? header.classList.add("top")
-      : header.classList.remove("top");
+      ? header.firstElementChild.classList.add('navFix')
+      : header.firstElementChild.classList.remove('navFix');
+    // entry.isIntersecting ? console.log(entry) : console.log(entry);
   };
 
-  const observer = nav._observer(callback, option);
-  observer.observe(header);
+  const observer = new IntersectionObserver(callback, option);
+  observer.observe(hero);
 };
 
 const init = function () {
-  nav._renderDefault("afterbegin");
-  footer._renderDefault("beforeend");
-  hero._renderDefault("afterbegin");
+  nav._renderDefault('afterbegin');
+  footer._renderDefault('beforeend');
+  hero._renderDefault('afterbegin');
   controlObserverNav();
 };
 init();
